5b08e677467edd139ab1aa749c3e49d96528dab3
Yoshino <weihaomouse@gmail.com>
2018-08-08 10:14:41 -0400

    Bot token

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import discord
 from discord.ext import commands
 
+import os # for importing env vars 
+
 description = '''This is a bot made for the Pawed.space\n
 For more detail, please contact http://t.me/allen0099'''
 
 bot = commands.Bot(command_prefix='!', description=description)
-token = '*'
+token = os.environ['Bot_Token']
 
 
 @bot.event
